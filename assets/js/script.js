a059f4ba034880fbe3094691f96c986f19c410b4
Tanvir Anjum Ratul <155052911+tanviranjumratul@users.noreply.github.com>
2023-12-28 22:19:12 +0600

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["Data Analysis", "Data Vizualization", "Web Scraping"],
+    strings: ["Data Analyst", "Data Vizualizer", "Web Scraper"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
